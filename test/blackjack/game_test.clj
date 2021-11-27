5e1ffd8894b1c1c6fa86f528e2d261a859630622
aristotelesbr <aristotelesbr@gmail.com>
2021-11-27 10:08:19 -0300

    test(game):ensure init player

--- test/blackjack/game_test.clj
+++ test/blackjack/game_test.clj
@@ -2,8 +2,16 @@
   (:require [clojure.test :refer :all]
             [blackjack.game :refer :all]))
 
-(deftest new-card-test
+(deftest test-new-card
   (testing "Must be generate a number gte 1"
     (is (>= (new-card) 1)))
   (testing "Must be generate a number lte 13"
     (is (<= (new-card) 13))))
+
+(deftest test-player
+  (testing "Must be return a valid players data"
+    (let [player-map (player "Arch")]
+      (is (= true (->
+                   (and
+                    (= (player-map :player-name) "Arch")
+                    (= (count (player-map :cards)) 2))))))))
